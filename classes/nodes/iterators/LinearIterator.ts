c60fdf1b9920f4d7366b0b7460213864645fb262
baptiste <baptiste.tiers@epitech.eu>
2020-11-05 08:17:14 +0100

    Timeout to see in action

--- classes/nodes/iterators/LinearIterator.ts
+++ classes/nodes/iterators/LinearIterator.ts
@@ -18,25 +18,27 @@ export default class LinearIterator extends ASTIteratorNode {
 
   launch(ast: CanvasAST, canvas: string[], setCanvas: (c: string[]) => any) {
     canvas.forEach((color, index) => {
-      const canvasColor = hexToRgb(color);
+      window.setTimeout(() => {
+        const canvasColor = hexToRgb(color);
 
-      if (!canvasColor) throw new Error('invalid_canvas');
+        if (!canvasColor) throw new Error('invalid_canvas');
 
-      this.node.canvasData.schema.outputs[0].value = new Pixel(
-        canvasColor[0],
-        canvasColor[1],
-        canvasColor[2],
-      );
+        this.node.canvasData.schema.outputs[0].value = new Pixel(
+          canvasColor[0],
+          canvasColor[1],
+          canvasColor[2],
+        );
 
-      if (ast.flow) {
-        this.traverse(ast.flow);
+        if (ast.flow) {
+          this.traverse(ast.flow);
 
-        let { r, g, b } = ast.out.node.canvasData.schema.inputs[0].value as Pixel;
+          let { r, g, b } = ast.out.node.canvasData.schema.inputs[0].value as Pixel;
 
-        canvas.splice(index, 1, rgbToHex(r, g, b));
+          canvas.splice(index, 1, rgbToHex(r, g, b));
 
-        setCanvas([...canvas]);
-      } else throw new Error('no_valid_flow');
+          setCanvas([...canvas]);
+        } else throw new Error('no_valid_flow');
+      }, index * 10);
     });
   }
 }
